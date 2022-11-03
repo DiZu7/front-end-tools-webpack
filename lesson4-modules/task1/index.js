79df8fd47600df05973de3b6879fa6b7efe1ae02
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-03 21:16:12 +0200

    added init files

---
+++ lesson4-modules/task1/index.js
@@ -0,0 +1,13 @@
+import calcExpression, { mult, sum } from './calculator/array-flat.js';
+import { printProfile } from './profile/index.js';
+import fetchUser from './profile/gateway.js';
+import './polyfills/array-flat.js';
+
+const calcResult = calcExpression('1 + 2');
+const sumResult = sum(1, 2);
+const multResult = mult(1, 2);
+const userDataPromise = fetchUser('facebook');
+printProfile({ name: 'Tom', from: 'The World'});
+
+
+
