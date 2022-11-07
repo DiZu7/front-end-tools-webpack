d5dd2a4493aff05bbc5ddb3cf39d9e76753490d6
DiZu7 <dianadovhanyuk@gmail.com>
2022-11-07 12:33:57 +0200

    test

--- lesson8-webpack-devserver/task1/webpack.config.js
+++ lesson8-webpack-devserver/task1/webpack.config.js
@@ -14,8 +14,9 @@ module.exports = (env, argv) => {
         {
           test: /.s?css$/,
           use: [
-            isProduction ? MiniCssExtractPlugin.loader : 
-				'style-loader',
+            isProduction 
+					? MiniCssExtractPlugin.loader 
+					: 'style-loader',
             'css-loader',
             'sass-loader',
           ],
